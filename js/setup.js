d7ef42a82cac6e0b4b807864847891fc05a965f1
alekseyburn <alekseyburn@gmail.com>
2020-06-05 21:58:45 +0300

    исправил именование массивов, добавил параметр template для функции createWizard

--- js/setup.js
+++ js/setup.js
@@ -4,19 +4,19 @@ var userDialog = document.querySelector('.setup');
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 function generateWizardsArray() {
   var wizards = [];
 
   for (var i = 0; i < 4; i++) {
     var obj = {
-      name: getRandomElement(NAMES) + ' ' + getRandomElement(SURNAMES),
-      coatColor: getRandomElement(COAT_COLORS),
-      eyesColor: getRandomElement(EYES_COLORS)
+      name: getRandomElement(names) + ' ' + getRandomElement(surnames),
+      coatColor: getRandomElement(coatColors),
+      eyesColor: getRandomElement(eyesColors)
     };
 
     wizards.push(obj);
@@ -28,8 +28,8 @@ function getRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function createWizard(wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+function createWizard(wizard, template) {
+  var wizardElement = template.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
@@ -42,7 +42,7 @@ function createWizards(wizards) {
   var fragment = document.createDocumentFragment();
 
   wizards.forEach(function (wizard) {
-    fragment.appendChild(createWizard(wizard));
+    fragment.appendChild(createWizard(wizard, similarWizardTemplate));
   });
 
   return fragment;
